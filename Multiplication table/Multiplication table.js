84a40f916198fed28cfcfde367b9f1a00b1df7a6
Harshawardhan Kamble <kambleharsh08@outlook.com>
2022-10-03 00:28:42 +0530

    modified

--- Multiplication table/Multiplication table.js
+++ Multiplication table/Multiplication table.js
@@ -2,5 +2,5 @@ const readlineSync=require("readline-sync")
 const n=readlineSync.question(`Enter a number s`)
 for(let i=1;i<=10;i++){
     let product=n*i;
-    console.log(n+" * "+i+" = "+product)
+    console.log(`${n} * ${i} = ${product}`)
 }
