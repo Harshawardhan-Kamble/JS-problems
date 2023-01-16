32f4da25e2be03b5e4c1d5a784518ab9b1f3bc37
Harshawardhan Kamble <kambleharsh08@outlook.com>
2023-01-16 19:40:32 +0530

    modified

--- Higherorderfunctions.js
+++ Higherorderfunctions.js
@@ -1,12 +1,36 @@
 const readLineSync=require("readline-sync");
 const inputArray=readLineSync.question("Enter array :");
 const arr=[...inputArray];
-function SquareofNumbers(){
-    let Square=[];
-    for(let numbers of arr){
-        Square.push(numbers**2)
+// function SquareofNumbers(){
+//     let Square=[];
+//     for(let numbers of arr){
+//         Square.push(numbers**2)
+// }
+//     return Square;
+// }
+// const squareOfInput=SquareofNumbers();
+// console.log(squareOfInput);
+// function CubeofNumbers(){
+//     let Cube=[];
+//     for(let numbers of arr){
+//         Cube.push(numbers**3)
+// }
+//     return Cube;
+// }
+// const cubeOfInput=CubeofNumbers();
+// console.log(cubeOfInput);
+function square(number){
+    return (number**2);
 }
-    return Square;
+function cube(number){
+    return (number**3);
 }
-const squareOfInput=SquareofNumbers();
-console.log(squareOfInput);
+function output (operation,arr){
+    let tempArray=[];
+    for (let number of arr){
+    tempArray.push(operation(number))
+}
+return tempArray;
+}
+const value=output(cube,arr);
+console.log(value)
